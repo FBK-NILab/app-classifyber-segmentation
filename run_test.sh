a1f50877ce5c5e93be9c217e3df14321c66bdda3
Giulia Bertò <giulia.berto.4@gmail.com>
2020-07-14 19:29:42 +0200

    save bundles native space

--- run_test.sh
+++ run_test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 subjID=`jq -r '._inputs[0].meta.subject' config.json`
-#static=`jq -r '.tractogram_static' config.json`
-static=track_aligned/track.tck
+static=`jq -r '.tractogram_static' config.json`
+static_mni=track_aligned/track.tck
 t1_static=`jq -r '.t1_static' config.json`
+t1_static_mni=MNI152_T1_1.25mm_brain.nii.gz
 
 echo "Check the inputs subject id"
 if [ ! $subjID == `jq -r '._inputs[1].meta.subject' config.json` ]; then
@@ -12,14 +13,13 @@ echo "Inputs subject id incorrectly inserted. Check them again."
 fi
 
 echo "Tractogram conversion to trk"
-python tck2trk.py $t1_static $static -f
-cp track_aligned/track.trk $subjID'_track.trk'
+python tck2trk.py $t1_static_mni $static_mni -f
 
 echo "Running Classifyber (only test)"
 mkdir -p tracts_trks
 python test_classifyber.py \
 		-src_dir 'results_training' \
-		-static $subjID'_track.trk' \
+		-static track_aligned/track.trk \
 		-out_dir 'tracts_trks'
 
 if [ -z "$(ls -A -- "tracts_trks")" ]; then    
